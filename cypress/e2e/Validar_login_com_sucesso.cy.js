2376b7fb76b9e17c5342986d43dc8f08dc3ffaa8
Mask014 <vinicius.lopes.9691@gmail.com>
2024-05-21 20:49:16 -0300

    subindo alteraç'ões

---
+++ cypress/e2e/Validar_login_com_sucesso.cy.js
@@ -0,0 +1,21 @@
+describe('Funcionalidade Cadastro', () => {
+  it('Validar login realiza com sucesso', () => {
+
+    //acessar a pagina
+    cy.visit('http://aprendendotestar.com.br/treinar-automacao.php')
+
+    //preencher os campos
+    cy.get(':nth-child(2) > td > input').type('vinicius')
+    cy.get(':nth-child(4) > td > input').type('senha')
+    cy.get(':nth-child(6) > td > input').type('mask014')
+
+    
+    //clicar no botão enviar 
+    cy.get('td > .btn').click()
+
+    //verificar se o cadastro foi realizado com sucesso
+    cy.get('tbody > :nth-child(2) > :nth-child(2)').should('have.text', 'mask014')
+
+  })
+}) 
+
